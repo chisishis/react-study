573f9225b552f2ed7a61d1d10d96e5b74700645a
Chris Chang <chris.chang.dev@gmail.com>
2019-07-28 20:29:18 -0600

    added cartIcon and cartDtopdown components to the app, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import './App.css';
 import { connect } from 'react-redux'
+
+import './App.css';
+
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth,  createUserProfileDocument } from './firebase/firebase.utils'
-import { setCurrentUser } from './redux/user/user.actions'
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-
     const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth =
-    auth.onAuthStateChanged(async userAuth => {
-      //this.setState({ currentUser : user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -30,10 +27,10 @@ class App extends React.Component {
             id: snapShot.id,
             ...snapShot.data()
           });
-        });         
+        });
       }
       else {
-        this.setState(userAuth);
+        setCurrentUser(userAuth);
       }
       
     });
@@ -69,13 +66,13 @@ class App extends React.Component {
 
 const mapStateToProps = ({ user }) => ({
   currentUser: user.currentUser
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(App);
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
